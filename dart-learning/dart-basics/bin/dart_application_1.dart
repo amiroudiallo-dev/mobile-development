55308b4f05eb4ba9703dc684ea7c79149692a132
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-05-13 18:34:53 +0000

    dart-basics

---
+++ dart-learning/dart-basics/bin/dart_application_1.dart
@@ -0,0 +1,16 @@
+
+
+void main() {
+  print("Hello, i a am beginer in dart!");
+  print('Hassan"s lesssons.');
+  print("Hassan's lesssons.");
+
+  print('''
+  
+  Hello
+  World!
+
+  ''');
+
+  print('3 + 5 = ${3 + 5}');
+}
