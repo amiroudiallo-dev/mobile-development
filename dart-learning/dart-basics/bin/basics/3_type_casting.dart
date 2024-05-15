c51831cac40c3c679b4a31873fab088b6f6eaf19
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-05-15 14:50:28 +0000

    3_type_casting.dart

---
+++ dart-learning/dart-basics/bin/basics/3_type_casting.dart
@@ -0,0 +1,16 @@
+
+
+void main() {
+
+  var x = 10;
+  var y = x.toDouble();
+
+  var booleanValue = false;
+
+  var num1 = '22';
+  var num2 = num.parse(num1);
+
+  print(num1.runtimeType);
+  print('x = $x');
+  print('y = $y');
+}
